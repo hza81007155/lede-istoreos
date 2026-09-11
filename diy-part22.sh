0f896a8bcdec6614d488a5af8f3af0e82ce15773
hza81007155 <800755@qq.com>
2026-09-11 21:39:01 +0800

    Add files via upload

---
+++ diy-part22.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#!/bin/bash
+
+# 移除要替换的包
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-dockerman
+rm -rf feeds/luci/applications/luci-app-lucky
+rm -rf feeds/luci/applications/luci-app-wol
+rm -rf feeds/luci/applications/luci-app-vlmcsd
+rm -rf feeds/luci/applications/luci-app-accesscontrol
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/geoview
+rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/lucky
+rm -rf feeds/packages/utils/coremark
+
+#更改默认地址为192.168.10.12
+sed -i 's/192.168.1.1/192.168.10.12/g' package/base-files/files/bin/config_generate
+
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by hza800755 /g" package/lean/default-settings/files/zzz-default-settings
+
+# istore
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
+git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/istore package/istore
+
+# partexp 扩容分区
+#git clone https://github.com/hza81007155/luci-app-partexp
+
+# Argone theme
+git clone --depth=1 -b master https://github.com/hza81007155/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 -b master https://github.com/hza81007155/luci-app-argon-config package/luci-app-argon-config
+
+# ddns-go
+git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-ddns-go.git
+
+#luci-app-mosdns
+git clone --depth=1 --single-branch https://github.com/sbwml/luci-app-mosdns.git
+
+# passwall/passwall2
+git clone --depth=1 --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall.git
+git clone --depth=1 --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall2.git
+
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+
+# adguardHome
+git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-adguardhome.git
+
+# cpufreq
+git clone --depth=1 --single-branch https://github.com/hza81007155/luci-app-cpufreq.git
+
+# lucky
+# git clone --depth=1 --single-branch https://github.com/gdy666/luci-app-lucky.git
+
+#Open Clash
+git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
+
+#预置OpenClash内核和GEO数据
+export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
+export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
+export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
+export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+
+export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
+export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+
+export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
+export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+
+cd ./OpenClash/luci-app-openclash/root/etc/openclash
+
+curl -sfL -o ./Country.mmdb $GEO_MMDB
+curl -sfL -o ./GeoSite.dat $GEO_SITE
+curl -sfL -o ./GeoIP.dat $GEO_IP
+
+mkdir ./core && cd ./core
+
+curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
+gzip -d ./tun.gz && mv ./tun ./clash_tun
+
+curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
+tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
+
+curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
+tar -zxf ./dev.tar.gz
+
+chmod +x ./clash* ; rm -rf ./*.gz
+
